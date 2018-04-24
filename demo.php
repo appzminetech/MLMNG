9ee3c410876b14284d8f6e68ecc7a77d834df0a5
appzminetech <38648445+appzminetech@users.noreply.github.com>
2018-04-24 12:20:49 +0530

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php include_once("header.php");?>
+<!-- Main content Starts here --> 
+<div class="content-wrapper">
+    <!-- Add Contents from here -->
+
+</div>
+<!-- Main content Ends here -->
+<?php include_once("footer.php"); ?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include_once("header.php");
+?>
+<!-- Main content Starts here -->
+ 
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Control panel</small>
+      </h1>
+      
+
+    </section>
+    <!-- /.content -->
+  </div>
+ 
+    <!-- Main content Ends here -->
+<?php
+include_once("footer.php");
+?>
